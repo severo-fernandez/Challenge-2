5d6bd8348cea96cae9286dbfbae2d1ecb9860136
Severo <severocf@gmail>
2022-04-24 13:22:13 -0500

    rework of CSV output

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
+        csvpath = Path("qualified_loans.csv")
         csvpath = questionary.text("Do you want to save the CSV file?").ask()
         csvpath = Path(csvpath)
         with open (csvpath, "w", newline="") as csvfile:
